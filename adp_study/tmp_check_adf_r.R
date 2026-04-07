6d4526226679c9268de720503aacd59f0358f164
맞갖다 <xoosl033110@gmail.com>
2026-04-07 17:22:37 +0900

    Remove temporary ADF check script

--- adp_study/tmp_check_adf_r.R
+++
@@ -1,9 +0,0 @@
-library(tseries)
-d <- read.csv("adp_study/data/timeseries_stationarity_daily.csv")
-d$date <- as.Date(d$date)
-d <- d[order(d$date), ]
-x <- ts(d$value, frequency = 7)
-cat("R raw ADF\n")
-print(adf.test(x))
-cat("R diff ADF\n")
-print(adf.test(diff(x)))
